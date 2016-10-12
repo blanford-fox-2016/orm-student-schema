9e8366ec9be7e8a97350d930ae19f252e9115c17
Toni & Dharmadi Tanamas <pair+chrisantoni.dharmadi93@hacktiv8.com>
2016-10-12 15:55:54 +0700

    orm student schema

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 "use strict"
+
+var models = require('./models')
+
+var Student = models.Student
+
+Student.create({
+  first_name: "axaxaxax",
+  last_name: "hehehhe",
+  gender: "Male",
+  age: 9,
+  email: "xaxaxaa@gmail.com",
+  phone: "1231231233"
+})
+
+// Student.update({
+//   age: 22,
+// }, {
+//   where: {
+//     age: null
+//   }
+// });
+// console.log(Student.getName());
+// Student.classMethods.
+Student.getName()
+// Student.getAge()
