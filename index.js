f6e462c7cb8c2b3abb177f9183de84745efb52ad
Juang Wiantoro & Mangku Widodo <pair+broerjuang.mangkuwi@devbootcamp.com>
2016-10-12 16:55:36 +0700

    realase four

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 "use strict"
+
+let models = require('./models');
+let Student = models.Students;
+
+
+Student.create({
+  firstname: "juan",
+  lastname: "budi",
+  age: 1,
+  email: "ahao@aiai.com",
+  phoneNumber: 12345678901
+}).catch( (err) => {
+    console.log("Insert the data correctly");
+});
+
+//Student.getName("mangku");
+// Student.validation("alalala@alala.com")
