c506a2c09d7fffdcb2641f66f5b6b75bdeb54b6b
yonisetiawan <yonisetiawan1990@gmail.com>
2016-10-12 15:41:15 +0700

    orm-student done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 "use strict"
+
+let models = require('./models');
+let Siswa = models.Student;
+
+Siswa.create({
+  gender:"cowok",
+  birthday:"1998-09-09",
+  email:"sfirst.amdddda@gmail.com",
+  phone:"12345678910",
+  age:"6",
+  nama:"Tiiiii"
+})
+
+
+
+// Siswa.getName()
+// Siswa.getAge()
+
+
+//rubah ini pada models/index.js jika menggunakan selain windows
+//  \config\config.json
+//var config    = require(__dirname + '/../config/config.json')[env];
---
+++ migrations/20161012032545-create-student.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Students', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.STRING
+      },
+      last_name: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.TEXT
+      },
+      birthday: {
+        type: Sequelize.DATE
+      },
+      email: {
+        type: Sequelize.TEXT
+      },
+      phone: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Students');
+  }
+};
