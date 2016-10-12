5f9155f4e9e5cf05a208f8c3fec6518a62a3e157
Septian A Tama <tama@tamatamvan.web.id>
2016-10-12 15:41:11 +0700

    release 0-4, by Tama

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 "use strict"
+let models = require('./models');
+let Student = models.Student;
+let Sequelize = require('Sequelize');
+
+let sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './db/init.sql'
+});
+
+// Student.create({
+//   first_name: "Ren",
+//   last_name: "Kiryu",
+//   birthday: "1998-03-02",
+//   gender: "female",
+//   email: "kiryuren@gmail.com",
+//   phone: "08988758832",
+//   age: 18
+// })
+
+Student.create({
+  first_name: "Bucky",
+  last_name: "Barnes",
+  birthday: "1901-07-04",
+  gender: "male",
+  email: "winter@soldi.er",
+  phone: "085740112321",
+  age: 115
+})
+
+Student.get_fullname(3)
+Student.get_age(3)
